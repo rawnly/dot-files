68330909f2875e14f8fea8efc69a7345af553c7c
Rawnly <fedevitale99@gmail.com>
2016-12-11 21:05:26 +0100

    update

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+echo "Hello $USER"
+echo "Now i'll install all dependecies and files.."
+
+sudo npm install -g weather-commandline
+
 if [[ $SHELL == '/bin/zsh' ]]
 then
   echo "Your are using $SHELL"
